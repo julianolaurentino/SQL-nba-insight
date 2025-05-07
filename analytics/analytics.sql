c0a4a1c8248cb9dbedca9de75e08cbacf5eaba7c
Juliano <julianolaurentino@gmail.com>
2025-05-06 23:46:44 -0300

    teste

--- analytics/analytics.sql
+++ analytics/analytics.sql
@@ -1,6 +1,6 @@
 SELECT name FROM sqlite_master WHERE type='table';
 
---Listar os 10 jogos mais recentes da temporada 2023-2024
+--Lista dos 10 jogos mais recentes da temporada 2023-2024
 SELECT
 	game_id 
 	,game_date 
@@ -11,3 +11,6 @@ FROM game
 WHERE game_date BETWEEN '2023-01-01' AND '2024-01-01'
 ORDER BY game_date DESC
 LIMIT 100
+
+
+
